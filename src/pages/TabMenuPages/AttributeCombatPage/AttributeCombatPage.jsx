4d03648d941dd7db883b6e01789884b9e2f1e123
LarissaBJ <larissabastosj@gmail.com>
2024-04-25 00:36:27 -0300

    CorreçãoImagens2

--- src/pages/TabMenuPages/AttributeCombatPage/AttributeCombatPage.jsx
+++ src/pages/TabMenuPages/AttributeCombatPage/AttributeCombatPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Life from '../../../components/Life/Life';
 
 const AttributeCombatPage = () => {
     return (
         <div>
             <h1>ATTRIBUTE COMBAT PAGE</h1>
+            <Life/>
         </div>
     );
 };
